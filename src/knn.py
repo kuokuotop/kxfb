b8938457b5d30d7723436913a6d138b24aabb180
kuokuotop <153844209@qq.com>
2018-01-29 13:27:08 +0800

    knn add
    kuokuotop add

--- src/knn.py
+++ src/knn.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from numpy import *
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+from sklearn.metrics import label_ranking_loss
 from Config import get_config
 config = get_config()
 
@@ -12,7 +13,7 @@ matrix = np.loadtxt(open(config.data_path + 'Test/' + 'Matrix_' + str(0.003) + '
 print matrix.shape
 
 label = []
-with open(config.data_path + 'Test/' + 'All electronic products_label.txt','r') as f:
+with open(config.data_path + 'Test/' + 'All electronic products_label.txt', 'r') as f:
     for line in f.readlines():
         label.append(int(line.strip()))
 f.close()
@@ -33,8 +34,10 @@ pp_y_predict = knn.predict(pp_x_test)
 score = knn.score(pp_x_test, pp_y_test, sample_weight=None) #准确率
 # neighborpoint = knn.kneighbors(pp_x_test[-1], 5, False) #计算与最后一个测试样本距离在最近的5个点，返回的是这些样本的序号组成的数组
 probility = knn.predict_proba(pp_x_test)  #计算各测试样本基于概率的预测
+print probility
+print len(probility)
 
-
+'''
 print('pp_y_predict = ')  
 print(pp_y_predict)  
 #输出测试的结果
@@ -45,7 +48,27 @@ print 'Accuracy:', score
 #输出准确率计算结果
 #print 'neighborpoint of last test sample:',neighborpoint
 #print pp_x[neighborpoint]
-print 'probility:',probility
+print 'probility:', probility
+
+'''
+i = 0
+PP_y_test = []
+for i in range(len(pp_y_test)):
+    if pp_y_test[i] == 1:
+        PP_y_test.append([0, 1])
+    else:
+        PP_y_test.append([1, 0])
+# print PP_y_test
+
+# y_true = np.array([[1, 0, 0], [0, 0, 1]])
+# y_score = np.array([[0.75, 0.5, 1], [1, 0.2, 0.1]])
+loss = label_ranking_loss(PP_y_test, probility)
+print loss, '+++'
+
+
+# y_score = np.array([[1.0, 0.1, 0.2], [0.1, 0.2, 0.9]])
+# print label_ranking_loss(pp_y_test, probility)
+
 
 # classify_report = metrics.classification_report(y_true, y_pred)
 # confusion_matrix = metrics.confusion_matrix(y_true, y_pred)
